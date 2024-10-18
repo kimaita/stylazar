efbd2f47537d214348f1f67c921a3be688b21786
Georgina Kimani <njokikimani001@gmail.com>
2024-10-18 17:23:09 +0300

    changed the fonts in header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,9 @@ import './header.css'
 export default function Header() {
   return (
     <div className="header">
-      <div className="headerTitles">
-        <span className="headerTitleSmall">Article</span>
-        <span className="headerTitleLarge">Blogs</span>
+      <div className="headerTitle">
+        <span className="headerTitleLarge">For You</span>
       </div>
-      <img 
-        className="headerImg"
-        src="../../assets/pic-about-01.jpg" 
-        alt=""
-      />
     </div>
   )
 }
