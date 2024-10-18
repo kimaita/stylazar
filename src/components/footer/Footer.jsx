e2c9726d10c50a8a7e27f782e3c14952d7620cd2
Georgina Kimani <njokikimani001@gmail.com>
2024-10-18 16:42:11 +0300

    changed the footer view

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,9 +5,7 @@ export default function Footer() {
   return (
       <div className="footer">
           <div className="footerLeft">
-                  <div className="footerList">All Rights Reserved</div>
-                  <div className="footerList">© 2024</div>
-                  <div className="footerList">STYLAZAR</div>
+              <div className="footerList">All Rights Reserved © 2024 STYLAZAR</div>
           </div>
           <div className="footerRight">
               <div className="footerRightList">
