47c64c7d5e7593c77204d7c21302cb45f21c868d
kimaita <johnnieqaym@gmail.com>
2024-10-30 14:01:56 +0300

    updated model

--- backend/models/post.py
+++ backend/models/post.py
@@ -3,6 +3,9 @@
 import uuid
 from datetime import datetime
 
+import pydantic
+import pymongo
+from beanie import Document
 from sqlmodel import Field, Relationship, SQLModel
 
 from .base_model import BaseModel, UpdatableModel
@@ -18,9 +21,8 @@ class Post(BaseModel, UpdatableModel, table=True):
     mongo_id: str
     title: str
     slug: str
-    display_excerpt: str | None
-    is_public: bool = Field(default=True)
-    is_published: bool = Field(default=True)
+    is_public: bool = Field(default=False)
+    is_published: bool = Field(default=False)
 
     author: User | None = Relationship(back_populates="posts")
     comments: list["Comment"] = Relationship(back_populates="post")
@@ -30,21 +32,18 @@ class Post(BaseModel, UpdatableModel, table=True):
 class PostCreate(SQLModel):
     """New post"""
 
-    title: str | None
+    title: str
     body: str | None
-    is_public: bool | None = None
-    is_published: bool | None = None
+    is_published: bool | None = False
 
 
 class PostUpdate(PostCreate):
     """Updating a post"""
 
-    id: uuid.UUID
-    # title: str | None = None
-    # body: str | None = None
+    title: str | None = None
+    body: str | None = None
     is_public: bool | None = None
     is_published: bool | None = None
-    updated_at: datetime = Field()
 
 
 class PostReaction(UpdatableModel, table=True):
@@ -58,3 +57,42 @@ class PostReaction(UpdatableModel, table=True):
 
     post: Post = Relationship(back_populates="reactions")
     # visitor : Visitor = Relationship()
+
+
+class PostVersion(pydantic.BaseModel):
+    """"""
+
+    version: int
+    content: str
+    published_at: datetime
+
+
+class PostPublic(SQLModel):
+    title: str
+    body: str
+    excerpt: str
+    slug: str
+    published_at: datetime
+    byline: str | None
+
+
+class PostDocument(Document):
+    """MongoDB document schema for a post"""
+
+    body: str | None
+    byline: str | None = None
+    excerpt: str | None = None
+    tags: list[str] | None = None
+    published_at: datetime | None = None
+    banner_image: str | None = None
+    # metadata:{word_count:int, read_time:timedelta?}
+    version_history: list[PostVersion] = None
+
+    class Settings:
+        # The name of the collection to store these objects.
+        name = "posts"
+        indexes = [
+            [
+                ("excerpt", pymongo.TEXT),
+            ],
+        ]
