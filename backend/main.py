6cc6d99503c9cac31c5de9a7050bc877b37cdcb2
kimaita <johnnieqaym@gmail.com>
2024-10-30 13:55:16 +0300

    added mongo client to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
 """app server module"""
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from api.main import api_router
 from core.config import settings
-from core.db import initialize_db
+from core.db import initialize_db, mongo_lifespan
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from middleware.middleware import RequestLoggingMiddleware
 
+
 app = FastAPI(
+    lifespan=mongo_lifespan,
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_PREFIX}/openapi.json",
 )
