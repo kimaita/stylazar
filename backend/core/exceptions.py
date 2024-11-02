29162068f7a9985add304fd5cf6babc14042762c
kimaita <johnnieqaym@gmail.com>
2024-11-02 05:12:23 +0300

    updated exceptions

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -14,3 +14,17 @@ def invalid_credentials(message: str | None = "Invalid user credentials"):
         detail=message,
         headers={"WWW-Authenticate": "Bearer"},
     )
+
+
+def invalid_img_file():
+    return HTTPException(
+        status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        detail="Unsupported file type. Allowed: jpg, jpeg, png, heic, heif",
+    )
+
+
+def exceeded_size():
+    return HTTPException(
+        status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+        detail="The image file is too large. Try 2MB or less",
+    )
