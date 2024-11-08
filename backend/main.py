9562d125cc150599dbb8a5266925cb6e7ede2c22
kimaita <johnnieqaym@gmail.com>
2024-11-09 02:21:51 +0300

    fix: adjusted allowed CORS origins

--- backend/main.py
+++ backend/main.py
@@ -7,17 +7,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middleware.request_logging import RequestLoggingMiddleware
 
-origins = [ "http://localhost", "http://localhost:3000", "http://localhost:8000" ]
 
 app = FastAPI(
     lifespan=mongo_lifespan,
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_PREFIX}/openapi.json",
 )
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://localhost:3000",
+    "https://127.0.0.1:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["origins"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
